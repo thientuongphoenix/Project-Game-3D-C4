46dfa8a099931dcc84fc249e8dbd585c7abbae37
thientuongphoenix <95729186+thientuongphoenix@users.noreply.github.com>
2025-04-18 11:43:08 +0700

    E20

---
+++ Assets/_Data/_Scripts/Enemy/EnemyMoving.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyMoving : MonoBehaviour
+{
+    public GameObject target;
+    public NavMeshAgent agent;
+
+    private void Start()
+    {
+        agent.SetDestination(target.transform.position);
+    }
+}
