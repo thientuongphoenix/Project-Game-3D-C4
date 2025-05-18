6c45eefc75d2a8a9611085862e48b0844f70bf51
thientuongphoenix <95729186+thientuongphoenix@users.noreply.github.com>
2025-05-18 15:14:20 +0700

    Create a left mouse button threshold to distinguish between Light Attack and Heavy Attack
    Create Debug Log for testing

--- Assets/_Data/Player/Skills/AttackHeavy.cs
+++ Assets/_Data/Player/Skills/AttackHeavy.cs
@@ -2,8 +2,13 @@ using UnityEngine;
 
 public class AttackHeavy : AttackAbstract
 {
+    protected void Update()
+    {
+        this.Attacking();
+    }
+
     protected override void Attacking()
     {
-        
+        if(InputManager.Instance.IsAttackHeavy()) Debug.Log("Attack Heavy");
     }
 }
--- Assets/_Data/Scripts/InputManager.cs
+++ Assets/_Data/Scripts/InputManager.cs
@@ -2,13 +2,18 @@ using UnityEngine;
 
 public class InputManager : SaiSingleton<InputManager>
 {
-    protected bool isAttackLight = false;
     protected bool isAiming = false;
+
+    protected float attackHold = 0;
+    protected float attackLightLimit = 0.5f;
+    protected bool isAttackLight = false;
+    protected bool isAttackHeavy = false;
+    
 
     void Update()
     {
         this.CheckAiming();
-        this.CheckAttackLight();
+        this.CheckAttacking();
     }
 
     protected virtual void CheckAiming()
@@ -16,9 +21,22 @@ public class InputManager : SaiSingleton<InputManager>
         this.isAiming = Input.GetMouseButton(1);
     }
 
-    protected virtual void CheckAttackLight()
+    protected virtual void CheckAttacking()
     {
-        this.isAttackLight = Input.GetMouseButtonUp(0);
+        if(Input.GetMouseButton(0)) this.attackHold += Time.deltaTime;
+
+        if(Input.GetMouseButtonUp(0))
+        {
+            this.isAttackLight = this.attackHold < this.attackLightLimit;
+            this.attackHold = 0;
+        }
+        else
+        {
+            this.isAttackLight = false;
+        }
+
+        if(this.attackHold > this.attackLightLimit) this.isAttackHeavy = true;
+        else this.isAttackHeavy = false;
     }
 
     public virtual bool IsAiming()
@@ -29,5 +47,10 @@ public class InputManager : SaiSingleton<InputManager>
     public virtual bool IsAttackLight()
     {
         return this.isAttackLight;
+    }
+
+    public virtual bool IsAttackHeavy()
+    {
+        return this.isAttackHeavy;
     }
 }
