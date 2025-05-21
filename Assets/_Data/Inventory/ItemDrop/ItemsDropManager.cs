802861429755b02556c2de853ae385a99e42dc9b
thientuongphoenix <95729186+thientuongphoenix@users.noreply.github.com>
2025-05-22 01:38:15 +0700

    Setup PoolObj for drop item

--- Assets/_Data/Inventory/ItemDrop/ItemsDropManager.cs
+++ Assets/_Data/Inventory/ItemDrop/ItemsDropManager.cs
@@ -1,6 +1,28 @@
+using System;
+using Unity.VisualScripting;
 using UnityEngine;
 
-public class ItemsDropManager : Spawner<ItemDropCtrl>
+public class ItemsDropManager : SaiSingleton<ItemsDropManager>
 {
-    
+    [SerializeField] protected ItemsDropSpawner spawner;
+    public ItemsDropSpawner Spawner => spawner;
+
+    protected override void LoadComponents()
+    {
+        base.LoadComponents();
+        this.LoadSpawner();
+    }
+
+    protected virtual void LoadSpawner()
+    {
+        if (this.spawner != null) return;
+        this.spawner = GetComponent<ItemsDropSpawner>();
+        Debug.Log(transform.name + ": LoadSpawner", gameObject);
+    }
+
+    public virtual void Drop(ItemCode itemCode, int dropCount)
+    {
+        // create item from pool
+        //make that item value
+    }
 }
