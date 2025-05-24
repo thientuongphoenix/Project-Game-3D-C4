1981bfbdfb06b66d385822e93ec73d4e523183c3
thientuongphoenix <95729186+thientuongphoenix@users.noreply.github.com>
2025-05-25 01:28:08 +0700

    Setup Obj, Folder & Scripts for Music & SFX

---
+++ Assets/_Data/Sounds/Music/MusicCtrl.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class MusicCtrl : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
