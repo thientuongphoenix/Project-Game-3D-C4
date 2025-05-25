73e5fc78ff28332e803fd1e01c485ab910100dc1
thientuongphoenix <95729186+thientuongphoenix@users.noreply.github.com>
2025-05-25 20:15:59 +0700

    Adjust wand drop number, clarify inventory in item drop manager

--- Assets/_Data/Enemy/_EnemyScripts/EnemyDamageReceiver.cs
+++ Assets/_Data/Enemy/_EnemyScripts/EnemyDamageReceiver.cs
@@ -65,7 +65,7 @@ public class EnemyDamageReceiver : DamageReceiver
         // item.itemCount = 1;
         // InventoryManager.Instance.Monies().AddItem(item);
         ItemsDropManager.Instance.DropMany(ItemCode.Gold, 10, transform.position);
-        ItemsDropManager.Instance.DropMany(ItemCode.Wand, 5, transform.position);
+        ItemsDropManager.Instance.DropMany(ItemCode.Wand, 1, transform.position);
         ItemsDropManager.Instance.DropMany(ItemCode.PlayerExp, 10, transform.position);
     }
 }
